7b5411cfa46da9aaf00595ad21ac6ab00e7bd649
sensui <sensuijunior@hotmail.com>
2020-09-06 16:09:16 -0300

    finished Hooks-App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MemoHook from './components/06-memos/MemoHook';
+import { Padre } from './components/07-tarea-memo/Padre';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+// import MemoHook from './components/06-memos/MemoHook';
 // import Memorize from './components/06-memos/Memorize';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { RealExempleRef } from './components/04-useRef/RealExempleRef';
@@ -15,7 +17,7 @@ import MemoHook from './components/06-memos/MemoHook';
 import './index.css';
 
 ReactDOM.render(
-  <MemoHook />,
+  <Padre />,
   document.getElementById('root')
 );
 
