3dc37fd1f538b8a5b911fb407373675c17dfb1c2
sensui <sensuijunior@hotmail.com>
2020-09-16 23:31:25 -0300

    useContext done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import MemoHook from './components/06-memos/MemoHook';
@@ -18,7 +19,7 @@ import { TodoApp } from './components/08-useReducer/TodoApp';
 import './index.css';
 
 ReactDOM.render(
-  <TodoApp />,
+  <MainApp />,
   document.getElementById('root')
 );
 
